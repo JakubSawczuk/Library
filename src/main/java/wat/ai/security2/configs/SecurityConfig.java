5813ed2972dd22175203853bdd441c4a65997197
JakubSawczuk <jakub.savczuk@gmail.com>
2019-01-03 22:05:22 +0100

    Update JWT token

--- src/main/java/wat/ai/security2/configs/SecurityConfig.java
+++ src/main/java/wat/ai/security2/configs/SecurityConfig.java
@@ -27,7 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+        http.cors()
+                .and()
+                .csrf()
                 .disable()
                 .addFilter(loginFilter())
                 .addFilter(jwtAuthorizer())
